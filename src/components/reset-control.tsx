dae311a1a73e9b9019df9db6d894a8d01eb0a0ae
why <3480874353@qq.com>
2026-01-27 23:17:34 +0800

    refactor resetControl

--- src/components/reset-control.tsx
+++ src/components/reset-control.tsx
@@ -1,4 +1,4 @@
-import { Button, CloseButton, Dialog, IconButton, Portal, useDialog, Text } from "@chakra-ui/react";
+import { Button, CloseButton, Dialog, IconButton, Portal, useDialog, Text, UseDialogReturn } from "@chakra-ui/react";
 import { useContext } from "react";
 import { LuRefreshCw } from "react-icons/lu";
 import { DrawStateContext } from "@/contexts/draw-state";
@@ -9,18 +9,11 @@ interface ResetControlProps {
 }
 
 export function ResetControl(props: ResetControlProps) {
-  const dialog = useDialog();
-  const { resetLevelMap, clearStickers } = useContext(DrawStateContext);
+ const dialog = useDialog();
   const rotatedProps = props.rotated ? {
     transform: "rotate(90deg)",
     transformOrigin: "center center",
   } : {};
-  const onResetButtonClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-    resetLevelMap();
-    clearStickers();
-    dialog.setOpen(false);
-    e.stopPropagation();
-  };
   return (
     <Dialog.RootProvider value={dialog} placement="center">
       <Dialog.Trigger asChild>
@@ -33,45 +26,17 @@ export function ResetControl(props: ResetControlProps) {
           <LuRefreshCw/> 重置
         </Button>
       </Dialog.Trigger>
-      <Portal>
-        <Dialog.Backdrop/>
-        <Dialog.Positioner>
-          <Dialog.Content {...rotatedProps}>
-            <Dialog.Header>
-              <Dialog.Title>请确认</Dialog.Title>
-            </Dialog.Header>
-            <Dialog.Body>
-              <Text textStyle="xl">清空地图数据和表情？</Text>
-            </Dialog.Body>
-            <Dialog.Footer>
-              <Dialog.ActionTrigger asChild>
-                <Button variant="outline">取消</Button>
-              </Dialog.ActionTrigger>
-              <Button onClick={onResetButtonClick}>确认</Button>
-            </Dialog.Footer>
-            <Dialog.CloseTrigger asChild>
-              <CloseButton size="sm" />
-            </Dialog.CloseTrigger>
-          </Dialog.Content>
-        </Dialog.Positioner>
-      </Portal>
+      <ResetDialogContent dialog={dialog} rotated={props.rotated}/>
     </Dialog.RootProvider>
   );
 }
 
 export function ResetControlWide(props: ResetControlProps) {
   const dialog = useDialog();
-  const { resetLevelMap, clearStickers } = useContext(DrawStateContext);
   const rotatedProps = props.rotated ? {
     transform: "rotate(90deg)",
     transformOrigin: "center center",
   } : {};
-  const onResetButtonClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-    resetLevelMap();
-    clearStickers();
-    dialog.setOpen(false);
-    e.stopPropagation();
-  };
   return (
     <Dialog.RootProvider value={dialog} placement="center">
       <Dialog.Trigger asChild>
@@ -85,28 +50,50 @@ export function ResetControlWide(props: ResetControlProps) {
           <LuRefreshCw/>
         </Button>
       </Dialog.Trigger>
-      <Portal>
-        <Dialog.Backdrop/>
-        <Dialog.Positioner>
-          <Dialog.Content {...rotatedProps}>
-            <Dialog.Header>
-              <Dialog.Title>请确认</Dialog.Title>
-            </Dialog.Header>
-            <Dialog.Body>
-              <Text textStyle="xl">清空地图数据和表情？</Text>
-            </Dialog.Body>
-            <Dialog.Footer>
-              <Dialog.ActionTrigger asChild>
-                <Button variant="outline">取消</Button>
-              </Dialog.ActionTrigger>
-              <Button onClick={onResetButtonClick}>确认</Button>
-            </Dialog.Footer>
-            <Dialog.CloseTrigger asChild>
-              <CloseButton size="sm" />
-            </Dialog.CloseTrigger>
-          </Dialog.Content>
-        </Dialog.Positioner>
-      </Portal>
+      <ResetDialogContent rotated={props.rotated} dialog={dialog}/>
     </Dialog.RootProvider>
+  );
+}
+
+interface ResetDialogContentProps {
+  dialog:UseDialogReturn
+  rotated?:boolean;
+}
+
+function ResetDialogContent(props: ResetDialogContentProps) {
+  const { resetLevelMap, clearStickers } = useContext(DrawStateContext);
+  const rotatedProps = props.rotated ? {
+    transform: "rotate(90deg)",
+    transformOrigin: "center center",
+  } : {};
+  const onResetButtonClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    resetLevelMap();
+    clearStickers();
+    props.dialog.setOpen(false);
+    e.stopPropagation();
+  };
+  return (
+    <Portal>
+      <Dialog.Backdrop/>
+      <Dialog.Positioner>
+        <Dialog.Content {...rotatedProps}>
+          <Dialog.Header>
+            <Dialog.Title>请确认</Dialog.Title>
+          </Dialog.Header>
+          <Dialog.Body>
+            <Text textStyle="xl">清空地图数据和表情？</Text>
+          </Dialog.Body>
+          <Dialog.Footer>
+            <Dialog.ActionTrigger asChild>
+              <Button variant="outline">取消</Button>
+            </Dialog.ActionTrigger>
+            <Button onClick={onResetButtonClick}>确认</Button>
+          </Dialog.Footer>
+          <Dialog.CloseTrigger asChild>
+            <CloseButton size="sm" />
+          </Dialog.CloseTrigger>
+        </Dialog.Content>
+      </Dialog.Positioner>
+    </Portal>
   );
 }
